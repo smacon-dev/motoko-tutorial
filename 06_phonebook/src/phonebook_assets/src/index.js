6455e2463e3f5ca71f81949c788ef0a4c6514c83
Sakanosita Moto <moto@nagamina.com>
2021-08-15 13:05:40 +0900

    Add phonebook

--- 06_phonebook/src/phonebook_assets/src/index.js
+++
@@ -1,9 +0,0 @@
-import { phonebook } from "../../declarations/phonebook";
-
-document.getElementById("clickMeBtn").addEventListener("click", async () => {
-  const name = document.getElementById("name").value.toString();
-  // Interact with phonebook actor, calling the greet method
-  const greeting = await phonebook.greet(name);
-
-  document.getElementById("greeting").innerText = greeting;
-});
