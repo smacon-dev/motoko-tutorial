347a428b8b3930fef1bab1fdefdcb8fcbe022241
Moto Sakanosita <moto@sakanosita.org>
2021-12-28 21:36:14 +0900

    update

--- 04_my_counter/src/my_counter_assets/src/index.js
+++
@@ -1,9 +0,0 @@
-import { my_counter } from "../../declarations/my_counter";
-
-document.getElementById("clickMeBtn").addEventListener("click", async () => {
-  const name = document.getElementById("name").value.toString();
-  // Interact with my_counter actor, calling the greet method
-  const greeting = await my_counter.greet(name);
-
-  document.getElementById("greeting").innerText = greeting;
-});
