2602ec38a920f899c8d5cad582c2aa9557f2a411
Moto Sakanosita <moto@sakanosita.org>
2021-12-29 18:15:05 +0900

    chore

--- 01_explore_hello/src/explore_hello_assets/src/index.js
+++ 01_explore_hello/src/explore_hello_assets/src/index.js
@@ -1,9 +1,19 @@
 import { explore_hello } from "../../declarations/explore_hello";
 
-document.getElementById("clickMeBtn").addEventListener("click", async () => {
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
   const name = document.getElementById("name").value.toString();
-  // Interact with explore_hello actor, calling the greet method
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
   const greeting = await explore_hello.greet(name);
 
+  button.removeAttribute("disabled");
+
   document.getElementById("greeting").innerText = greeting;
+
+  return false;
 });
