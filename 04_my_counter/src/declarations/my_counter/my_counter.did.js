b6fd71edaaf7ad730cc7f4a0c924cf9bf0f2b0d3
Moto Sakanosita <moto@sakanosita.org>
2021-12-29 21:17:37 +0900

    chore

--- 04_my_counter/src/declarations/my_counter/my_counter.did.js
+++
@@ -1,8 +0,0 @@
-export const idlFactory = ({ IDL }) => {
-  return IDL.Service({
-    'get' : IDL.Func([], [IDL.Nat], ['query']),
-    'increment' : IDL.Func([], [], []),
-    'set' : IDL.Func([IDL.Nat], [], []),
-  });
-};
-export const init = ({ IDL }) => { return []; };
