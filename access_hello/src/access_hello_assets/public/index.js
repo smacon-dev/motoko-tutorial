bded15ac59496acdeef8c071db241e1dad65cc04
Moto Sakanosita <moto@sakanosita.org>
2022-01-03 19:26:44 +0900

    chore

--- access_hello/src/access_hello_assets/public/index.js
+++
@@ -1,5 +0,0 @@
-import access_hello from 'ic:canisters/access_hello';
-
-access_hello.greet(window.prompt("Enter your name:")).then(greeting => {
-  window.alert(greeting);
-});
---
+++ access_hello/src/access_hello_assets/src/index.js
@@ -0,0 +1,19 @@
+import { access_hello } from "../../declarations/access_hello";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await access_hello.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
